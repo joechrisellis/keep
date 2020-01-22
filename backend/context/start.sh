ed24a3dc110298e06cc57cb2d42ee13abe427cda
Joe Ellis <joe.ellis@arm.com>
2020-01-22 23:05:55 +0000

    Run uwsgi as root

--- backend/context/start.sh
+++ backend/context/start.sh
@@ -2,4 +2,4 @@
 
 flask db upgrade
 
-uwsgi --socket 0.0.0.0:3031 --uid uwsgi --plugins python3 --protocol uwsgi --wsgi wsgi:app
+uwsgi --socket 0.0.0.0:3031 --plugins python3 --protocol uwsgi --wsgi wsgi:app
